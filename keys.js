ae5a4541d8b5cbf64c7da19e1216cb2f45e93917
Phil Mlaker <philmlaker@gmail.com>
2017-01-13 15:04:43 -0600

    movie and twitter function

--- keys.js
+++ keys.js
@@ -1,8 +1,7 @@
-console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'IK6AUKOZlX1IFpg4SM9v9T0c1',
-  consumer_secret: 'IK6AUKOZlX1IFpg4SM9v9T0c1',
+  consumer_key: 'nE45EFkkhazvAILWJ3mGr71Pm',
+  consumer_secret: 'F5VeRzRtof6M1VyhwImhEi4k3hvyO7kGe70BWDHLDEVgnbfCml',
   access_token_key: '3230272272-ycJFrmbfIrtsf86qpBa5EgZd9XHnujulxbCIdCp',
   access_token_secret: 'dmtM6AvaMyT5a9L64sciXcyWhJA9BPejDC6abJi1MVSfZ',
 };
--- liri.js
+++ liri.js
@@ -1,24 +1,64 @@
-var client = require("./keys.js");
-var Twitter = require('twitter');
-var queryURL = 'https://api.twitter.com/1.1/search/tweets.json?q=philmlaker';
-
-// Make it so liri.js can take in one of the following commands:
-
-// spotify-this-son
-// movie-this
-// do-what-it-says
-
-
-// my-tweets
-
-
-
+var keys = require("./keys.js");
+var request = require("request");
 
 input = process.argv[2];
+movieName = process.argv[3];
 
 if (input === "my-tweets") {
- 	console.log(JSON.parse(queryURL));
+
+    var Twitter = require('twitter');
+
+    var client = new Twitter({
+        consumer_key: keys.twitterKeys.consumer_key,
+        consumer_secret: keys.twitterKeys.consumer_secret,
+        access_token_key: keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterKeys.access_token_secret
+    });
+
+    client.get('statuses/user_timeline', function(error, tweets, response) {
+        if (error) throw error;
+        for (var i = 0; i < 20; i++) {
+            if (tweets[i] == undefined) { console.log('NO TWEET!') } else { console.log(tweets[i].text) }
+        }
+    });
+
+}
+
+// movie-this
+
+if (input === "movie-this") {
+
+    if (movieName == "undefined") {
+        console.log("help");
+        return;
+    }
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json";
+    request(queryUrl, function(error, response, body) {
+
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+
+
+                // Parse the body of the site and recover just the imdbRating
+                // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Year: " + JSON.parse(body).Year);
+                console.log("Rating: " + JSON.parse(body).Rated);
+                console.log("Country: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+            };
+            // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+            // console.log("Rottem Tomatoes URL: " + JSON.parse(body).Website);
+
+    });
 
 
 
 }
+
+// spotify-this-son
+// do-what-it-says
