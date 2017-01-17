1f49d309180bb74ef43222bcc6ad537fc456ea5d
Phil Mlaker <philmlaker@gmail.com>
2017-01-16 19:47:42 -0600

    Mon 7:47PM

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 var keys = require("./keys.js");
 var request = require("request");
- var spotify = require('spotify');
+
+var fs = require("fs");
+var cmd = require('node-cmd');
 
 var input = process.argv[2];
 var nodeArgs = process.argv;
@@ -31,9 +33,12 @@ if (input === "my-tweets") {
 
 if (input === "movie-this") {
 
-    if (movieName == null) {
-        var queryUrl = "http://www.omdbapi.com/?t=" + "Mr.Nobody" + "&y=&plot=short&r=json";
+    http: //www.omdbapi.com/?t=" + "Mr.Nobody" + "&tomatoes=false;
+
+        if (movieName == null) {
+        var queryUrl = "http://www.omdbapi.com/?t=" + "Mr.Nobody" + "&y=&plot=short&tomatoes=true&r=json";
         request(queryUrl, function(error, response, body) {
+
 
             // If the request is successful
             if (!error && response.statusCode === 200) {
@@ -49,10 +54,13 @@ if (input === "movie-this") {
                 console.log("Language: " + JSON.parse(body).Language);
                 console.log("Plot: " + JSON.parse(body).Plot);
                 console.log("Actors: " + JSON.parse(body).Actors);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+                console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL)
             };
         });
     } else {
-        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json";
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&r=json";
         request(queryUrl, function(error, response, body) {
 
             // If the request is successful
@@ -69,6 +77,8 @@ if (input === "movie-this") {
                 console.log("Language: " + JSON.parse(body).Language);
                 console.log("Plot: " + JSON.parse(body).Plot);
                 console.log("Actors: " + JSON.parse(body).Actors);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+                console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL)
             };
         });
     }
@@ -76,61 +86,67 @@ if (input === "movie-this") {
 
 
 if (input === "spotify-this-song") {
+    var spotify = require('spotify');
 
-    if( process.argv[3] == null){
-    var songName = "The Sign Ace of Base";
-    spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (process.argv[3] == null) {
+        var songName = "The Sign Ace of Base";
+        spotify.search({ type: 'track', query: songName }, function(err, data) {
 
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-    // var data = JSON.stringify(data);
-    console.log("Artist's Name: " + data.tracks.items[0].artists[0].name); 
-    console.log("Song Name: " + data.tracks.items[0].name); 
-    console.log("Preview Link: " + data.tracks.items[0].preview_url); 
-    console.log("Album Name: " + data.tracks.items[0].album.name); 
-});
-
-
-
-
-
-
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+            // var data = JSON.stringify(data);
+            console.log("Artist's Name: " + data.tracks.items[0].artists[0].name);
+            console.log("Song Name: " + data.tracks.items[0].name);
+            console.log("Preview Link: " + data.tracks.items[0].preview_url);
+            console.log("Album Name: " + data.tracks.items[0].album.name);
+        });
 
     } else {
 
-    var spotify = require('spotify');
-    song = process.argv[3];
+        var spotify = require('spotify');
+        song = process.argv[3];
 
-    for (var i = 3; i < nodeArgs.length; i++) {
+        for (var i = 3; i < nodeArgs.length; i++) {
 
-        if (i > 3 && i < nodeArgs.length) {
+            if (i > 3 && i < nodeArgs.length) {
 
-            song = song + "+" + nodeArgs[i];
+                song = song + "+" + nodeArgs[i];
 
-        } else {
+            } else {
 
-
+            }
 
         }
 
-    }
+        var songName = song;
+        spotify.search({ type: 'track', query: songName }, function(err, data) {
 
-    
-   var songName = song;
-    spotify.search({ type: 'track', query: songName }, function(err, data) {
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+            // var data = JSON.stringify(data);
+            console.log("Artist's Name: " + data.tracks.items[0].artists[0].name);
+            console.log("Song Name: " + data.tracks.items[0].name);
+            console.log("Preview Link: " + data.tracks.items[0].preview_url);
+            console.log("Album Name: " + data.tracks.items[0].album.name);
+        });
 
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-    // var data = JSON.stringify(data);
-    console.log("Artist's Name: " + data.tracks.items[0].artists[0].name); 
-    console.log("Song Name: " + data.tracks.items[0].name); 
-    console.log("Preview Link: " + data.tracks.items[0].preview_url); 
-    console.log("Album Name: " + data.tracks.items[0].album.name); 
-});
-
+    };
 };
-};
+
+
+if (input === "do-what-it-says") {
+
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        var  command = data.split(" ");
+        console.log(command[0]);
+
+
+
+    })
+
+}
