c96e20432a44495d943b0948747e31f930ef6cd2
Phil Mlaker <philmlaker@gmail.com>
2017-01-17 20:25:24 -0600

    Finished

--- liri.js
+++ liri.js
@@ -1,35 +1,30 @@
 var keys = require("./keys.js");
 var request = require("request");
-
 var fs = require("fs");
-var cmd = require('node-cmd');
-
 var input = process.argv[2];
 var nodeArgs = process.argv;
 var movieName = process.argv[3];
 
 
 
-
 if (input === "my-tweets") {
-
     var Twitter = require('twitter');
-
     var client = new Twitter({
         consumer_key: keys.twitterKeys.consumer_key,
         consumer_secret: keys.twitterKeys.consumer_secret,
         access_token_key: keys.twitterKeys.access_token_key,
         access_token_secret: keys.twitterKeys.access_token_secret
     });
-
     client.get('statuses/user_timeline', function(error, tweets, response) {
         if (error) throw error;
         for (var i = 0; i < 20; i++) {
             if (tweets[i] == undefined) { console.log('NO TWEET!') } else { console.log(tweets[i].text) }
         }
     });
-
 };
+
+
+
 
 if (input === "movie-this") {
 
@@ -58,6 +53,7 @@ if (input === "movie-this") {
                 console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
                 console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL)
             };
+
         });
     } else {
         var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&r=json";
@@ -85,8 +81,13 @@ if (input === "movie-this") {
 };
 
 
+
+
+
+
+
 if (input === "spotify-this-song") {
-    var spotify = require('spotify');
+
 
     if (process.argv[3] == null) {
         var songName = "The Sign Ace of Base";
@@ -138,15 +139,68 @@ if (input === "spotify-this-song") {
 };
 
 
+
+
+
+
+
+
+
+
 if (input === "do-what-it-says") {
-
-
     fs.readFile("random.txt", "utf8", function(error, data) {
-        var  command = data.split(" ");
-        console.log(command[0]);
+                var command = data.split(" ");
+
+                console.log(command[0]);
+                if (command[0] == "spotify-this-song") {
+                    console.log("yes!")
+                    console.log(command[1]);
+                    var songName = command[1];
+                    var spotify = require('spotify');
+
+                    spotify.search({ type: 'track', query: songName }, function(err, data) {
+
+                        if (err) {
+                            console.log('Error occurred: ' + err);
+                            return;
+                        }
+                        // var data = JSON.stringify(data);
+                        console.log("Artist's Name: " + data.tracks.items[0].artists[0].name);
+                        console.log("Song Name: " + data.tracks.items[0].name);
+                        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+                        console.log("Album Name: " + data.tracks.items[0].album.name);
+                    });
+
+                } else if (command[0] === "movie-this") {
+                     var movieName = command[1];
+                    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&r=json";
+                    request(queryUrl, function(error, response, body) {
+
+                        if (!error && response.statusCode === 200) {
+                            // Parse the body of the site and recover just the imdbRating
+                            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+
+                            console.log("Title: " + JSON.parse(body).Title);
+                            console.log("Year: " + JSON.parse(body).Year);
+                            console.log("Rating: " + JSON.parse(body).Rated);
+                            console.log("Country: " + JSON.parse(body).Country);
+                            console.log("Language: " + JSON.parse(body).Language);
+                            console.log("Plot: " + JSON.parse(body).Plot);
+                            console.log("Actors: " + JSON.parse(body).Actors);
+                            console.log("Actors: " + JSON.parse(body).Actors);
+                            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+                            console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL)
+                        };
 
 
 
-    })
 
+                    });
+
+
+
+
+                }
+
+});
 }
